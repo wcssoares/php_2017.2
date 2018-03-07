691f59e04c4d488fc4ebe53785a70c8af996ef04
51115152017.2 <51115152017.2@51DE194283.senacrjedu.local>
2018-03-06 22:03:22 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+    Autor: Willian Souza
+    Local: Senac Niterói
+    Data: 06-03-2018
+    Finalidade: Página principal do projeto
+-->
+<html>
+    <head lang="pt-br">
+        <meta charset="UTF-8">
+        <title>Aula 02</title>
+    </head>
+    <body>
+        <?php
+            $var1 = 10;
+            $var2 = 20;
+            $var3 = $var1 + $var2;
+            
+            echo $var3. "<br>";
+        ?>
+    </body>
+</html>
---
+++ tipo_dados.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+ * Autor: Willian Souza
+ * Data: 06-03-2018
+ * Finalidade: Explanação sobre os tipos de dados em PHP
+ */
+
+   # Variavel armazenando um valor texto
+   $texto = "Segunda aula de PHP";
+   # Variavel armazenando um valor inteiro
+   $inteiro = 02;
+   # Variavel armazenando um valor real
+   $real = 45.15;
+   # Variavel armazenando um valor caractere
+   $letra = 'L';
+   #Variavel armazenando um valor boleano
+   $boleano = TRUE;
+   
+   var_dump($texto);
+   echo "<br>";
+   var_dump($inteiro);
+   echo "<br>";
+   var_dump($real);
+   echo "<br>";
+   var_dump($letra);
+   echo "<br>";
+   var_dump($boleano);
+   
